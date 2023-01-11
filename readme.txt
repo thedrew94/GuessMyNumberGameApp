4ef3eb96f88225f0be6dd38beb69992975ab948c
thedrew94 <121413180+thedrew94@users.noreply.github.com>
2023-01-11 10:09:42 +0900

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,11 @@
+(Java Script) Here's a breakdown of what the code is doing:
+
+This app is a number guessing game where the player has to guess a randomly
+generated number between 1 and 20 within a certain number of tries. 
+The player starts with a score of 20 and their score decreases by 1 
+with each incorrect guess. 
+If the player correctly guesses the secret number, 
+they win the game and their score is compared to the highscore. 
+The player can then play the game again by clicking the "play again" 
+button, which resets the secret number and score to their 
+default values.
---
+++ script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const displayScore = message => {
+  document.querySelector('.score').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  //When there is no input
+  if (!guess) {
+    displayMessage('No number!');
+
+    //When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    //When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too High!' : 'Too Low!');
+      score--;
+      displayScore(score);
+    } else {
+      displayMessage('You lost the Game!');
+      displayScore('0');
+    }
+  }
+});
+//play again function, reset values to default
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  displayScore(score);
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+});
+
+
